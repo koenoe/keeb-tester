5cebb5389c74e5298aaa286c407efcac06c75ebd
Koen Romers <info@koenromers.com>
2019-05-20 16:51:56 +0100

    remove unneeded injectedReducers

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -4,10 +4,9 @@ import { connectRouter } from 'connected-react-router';
 
 import history from 'utils/history.js';
 
-export default function createReducer(injectedReducers: any = {}) {
+export default function createReducer() {
   const rootReducer: any = combineReducers({
     router: connectRouter(history),
-    ...injectedReducers,
   });
 
   return rootReducer;
