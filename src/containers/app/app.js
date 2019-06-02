101bc4c49cd3be04bf254b690e646dbe92ecad43
Koen Romers <info@koenromers.com>
2019-06-02 16:29:58 +0100

    properly type keyboard data

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -2,11 +2,13 @@
 import React from 'react';
 
 import KeyboardLayoutEditorInput from 'containers/keyboardLayoutEditorInput/keyboardLayoutEditorInput.js';
+import Keyboard from 'containers/keyboard/keyboard.js';
 
 export default function App() {
   return (
     <>
       <KeyboardLayoutEditorInput />
+      <Keyboard />
     </>
   );
 }
--- src/state/keyboard/state.js
+++ src/state/keyboard/state.js
@@ -1,14 +1,52 @@
 // @flow
-type Background = $ReadOnly<{|
+type KeyboardBackground = $ReadOnly<{|
   name: string,
   style: string,
 |}>;
 
-export type Keycaps = Array<Array<string | Object>>;
+type KeycapLegendAlignment =
+  | 'top-left'
+  | 'bottom-left'
+  | 'top-right'
+  | 'bottom-right'
+  | 'front-left'
+  | 'front-right'
+  | 'center-left'
+  | 'center-right'
+  | 'top-center'
+  | 'center'
+  | 'bottom-center'
+  | 'front-center';
 
+type KeycapLegend = $ReadOnly<{|
+  label: string,
+  alignment: KeycapLegendAlignment,
+|}>;
+
+export type Keycap = $ReadOnly<{|
+  backgroundColor: string,
+  color: string,
+  fontHeight2: number,
+  fontHeight: number,
+  ghosted: boolean,
+  height: number,
+  height2?: number,
+  homing: boolean,
+  legends: $ReadOnlyArray<KeycapLegend>,
+  profile: string,
+  stepped: boolean,
+  width: number,
+  width2?: number,
+  x: number,
+  x2?: number,
+  y: number,
+  y2?: number,
+|}>;
+
+export type Keycaps = $ReadOnlyArray<Keycap>;
 export type KeyboardState = $ReadOnly<{|
   author?: string,
-  background?: Background,
+  background?: KeyboardBackground,
   backgroundColor?: string,
   borderRadius?: string,
   keycaps: Keycaps,
