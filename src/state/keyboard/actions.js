3c12541799dec349d825abd537c8a1c9e47b60e3
Koen Romers <info@koenromers.com>
2019-06-02 16:59:24 +0100

    put logic for json parser in reducer

--- src/state/keyboard/actions.js
+++ src/state/keyboard/actions.js
@@ -1,37 +1,7 @@
 // @flow
-import type { KeyboardState } from './state.js';
-
 export const SET_KEYBOARD = 'keyboard/SET_KEYBOARD';
 export const SET_KEYBOARD_FROM_JSON = 'keyboard/SET_KEYBOARD_FROM_JSON';
 export const RESET_KEYBOARD = 'keyboard/RESET_KEYBOARD';
-
-export type SetKeyboardPayload = {|
-  ...KeyboardState,
-|};
-
-export type SetKeyboardAction = {|
-  type: 'keyboard/SET_KEYBOARD',
-  ...SetKeyboardPayload,
-|};
-
-export function setKeyboard({
-  author,
-  background,
-  backgroundColor,
-  borderRadius,
-  keycaps,
-  name,
-}: SetKeyboardPayload): SetKeyboardAction {
-  return {
-    type: SET_KEYBOARD,
-    author,
-    background,
-    backgroundColor,
-    borderRadius,
-    keycaps,
-    name,
-  };
-}
 
 export type SetKeyboardFromJsonAction = {|
   type: 'keyboard/SET_KEYBOARD_FROM_JSON',
@@ -57,7 +27,4 @@ export function resetKeyboard(): ResetKeyboardAction {
   };
 }
 
-export type Action =
-  | SetKeyboardAction
-  | SetKeyboardFromJsonAction
-  | ResetKeyboardAction;
+export type Action = SetKeyboardFromJsonAction | ResetKeyboardAction;
--- src/state/keyboard/reducer.js
+++ src/state/keyboard/reducer.js
@@ -1,8 +1,33 @@
 // @flow
 import * as actions from './actions.js';
 import { initialState as initialKeyboardState } from './state.js';
-import type { KeyboardState } from './state.js';
+import type { KeyboardState, Keycaps } from './state.js';
 import type { Action } from './actions.js';
+
+function extractKeycapsFromRows(
+  rows: $ReadOnlyArray<Array<string | Object>>,
+): Keycaps {
+  console.log(rows);
+  return [];
+}
+
+function extractKeyboardFromJson(rawJson: string): KeyboardState {
+  const json = JSON.parse(rawJson);
+  const rows = json.filter(row => row instanceof Array);
+  const firstRow = json.shift();
+  const keycaps: Keycaps = extractKeycapsFromRows(rows);
+
+  return {
+    ...(firstRow.author && { author: firstRow.author }),
+    ...(firstRow.background && { background: firstRow.background }),
+    ...(firstRow.backcolor && {
+      backgroundColor: firstRow.backcolor,
+    }),
+    ...(firstRow.radii && { borderRadius: firstRow.radii }),
+    ...(firstRow.name && { name: firstRow.name }),
+    keycaps,
+  };
+}
 
 export default function keyboardReducer(
   state: KeyboardState = initialKeyboardState,
@@ -13,7 +38,7 @@ export default function keyboardReducer(
   }
 
   switch (action.type) {
-    case actions.SET_KEYBOARD: {
+    case actions.SET_KEYBOARD_FROM_JSON: {
       const {
         author,
         background,
@@ -21,7 +46,8 @@ export default function keyboardReducer(
         borderRadius,
         keycaps,
         name,
-      } = action;
+      }: KeyboardState = extractKeyboardFromJson(action.rawJson);
+
       return {
         ...state,
         author,
@@ -32,8 +58,10 @@ export default function keyboardReducer(
         name,
       };
     }
+
     case actions.RESET_KEYBOARD:
       return initialKeyboardState;
+
     default:
       return state;
   }
